be7b53e7623e8844053269a04e5284b6db0a8b07
apisak.k <>
2019-10-31 14:46:48 +0700

    learning factory model

--- app/User.php
+++ app/User.php
@@ -32,10 +32,7 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
-    public function setTitleAttribute($value) {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);
-    }
+    
 
     /**
      * The attributes that should be cast to native types.
