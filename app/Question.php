61858dd49b3e74c12b703a431d170ce0a3c8e267
apisak.k <>
2019-11-13 15:46:15 +0700

    lesson-25 commit 01

--- app/Question.php
+++ app/Question.php
@@ -38,7 +38,7 @@ class Question extends Model
     }
 
     public function getBodyHtmlAttribute() {
-        return \Parsedown::instance()->text( $this->body );
+        return $this->bodyHtml();
     }
 
     public function answers() {
@@ -70,5 +70,19 @@ class Question extends Model
         return $this->favorites->count();
     }
 
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+
+    public function excerpt($length)
+    {   
+        return str_limit( strip_tags($this->bodyHtml()),$length);
+    }
+
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text( $this->body );
+    }
    
 }
